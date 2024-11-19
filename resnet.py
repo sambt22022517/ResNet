76b31221e8a9b081654eae23bded170081631b23
Bùi Tiến Sâm 22022517 <120190847+sambt22022517@users.noreply.github.com>
2024-11-19 21:51:32 +0700

    Add files via upload

--- resnet.py
+++ resnet.py
@@ -1,75 +1,85 @@
 import torch
 from torch import nn
 
+class MyConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding, stride, relu=True):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.stride = stride
+        self.relu = relu
+
+        model = [
+            nn.Conv2d(self.in_channels, self.out_channels, kernel_size=self.kernel_size, padding=self.padding, stride=self.stride),
+            nn.BatchNorm2d(self.out_channels),
+        ]
+        if self.relu:
+            model.append(nn.ReLU())
+        self.model = nn.Sequential(*model)
+    
+    def forward(self, X):
+        return self.model(X)
+    
 class Residual_Basic_Block(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, stride):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.stride = stride
 
         self.model = nn.Sequential(
-            nn.Conv2d(self.in_channels, self.out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(self.out_channels),
-
-            nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(self.out_channels),
+            MyConv(self.in_channels, self.out_channels, kernel_size=3, padding=1, stride=stride, relu=True),
+            MyConv(self.out_channels, self.out_channels, kernel_size=3, padding=1, stride=1, relu=False),
         )
 
-        if self.in_channels != self.out_channels:
+        if self.in_channels != self.out_channels or stride != 1:
             self.downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1),
-                nn.BatchNorm2d(self.out_channels)
+                MyConv(self.in_channels, self.out_channels, kernel_size=1, padding=0, stride=stride, relu=False),
             )
         else:
             self.downsample = nn.Identity()
 
     def forward(self, X):
-        skip_connect_X = X.clone()
-        downsample_X = self.downsample(skip_connect_X)
+        X_skip = X
+        X_skip = self.downsample(X_skip)
+        out = self.model(X)
+        out = out + X_skip
+        return out
 
-        X = self.model(X)
-
-        X = X + downsample_X
-        return X
-    
 class Residual_Bottle_Neck_Block(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, stride):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.stride = stride
 
         self.model = nn.Sequential(
-            nn.Conv2d(self.in_channels, self.out_channels // 4, kernel_size=1),
-            nn.BatchNorm2d(self.out_channels // 4),
-
-            nn.Conv2d(self.out_channels // 4, self.out_channels // 4, kernel_size=3, padding=1),
-            nn.BatchNorm2d(self.out_channels // 4),
-
-            nn.Conv2d(self.out_channels // 4, self.out_channels, kernel_size=1),
-            nn.BatchNorm2d(self.out_channels),
+            MyConv(self.in_channels, self.out_channels // 4, kernel_size=1, padding=0, stride=stride, relu=True),
+            MyConv(self.out_channels // 4, self.out_channels // 4, kernel_size=3, padding=1, stride=1, relu=True),
+            MyConv(self.out_channels // 4, self.out_channels, kernel_size=1, padding=0, stride=1, relu=False),
         )
 
-        if self.in_channels != self.out_channels:
+        if self.in_channels != self.out_channels or stride != 1:
             self.downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1),
-                nn.BatchNorm2d(self.out_channels)
+                MyConv(self.in_channels, self.out_channels, kernel_size=1, padding=0, stride=stride, relu=False),
             )
         else:
             self.downsample = nn.Identity()
 
     def forward(self, X):
-        skip_connect_X = X.clone()
-        downsample_X = self.downsample(skip_connect_X)
-
-        X = self.model(X)
-
-        X = X + downsample_X
-        return X
+        X_skip = X
+        X_skip = self.downsample(X_skip)
+        out = self.model(X)
+        out = out + X_skip
+        return out
     
-def make_block(block, in_channels, out_channels, num_blocks):
+def make_block(block, in_channels, out_channels, num_blocks, stride):
     model = nn.Sequential(
-        block(in_channels, out_channels),
-        *[block(out_channels, out_channels) for _ in range(num_blocks-1)]
+        block(in_channels, out_channels, stride),
+        *[block(out_channels, out_channels, 1) for _ in range(num_blocks-1)]
     )
     return model
 
@@ -83,37 +93,21 @@ class ResNet(nn.Module):
             expansion_size = 1
 
         self.conv1 = nn.Conv2d(self.in_channels, 64, kernel_size=7, stride=2, padding=3)
-        
-        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv2_x = make_block(self.block, 64, 64*expansion_size, list_num_residual_blocks[0])
-        
-        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv3_x = make_block(self.block, 64*expansion_size, 128*expansion_size, list_num_residual_blocks[1])
-        
-        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv4_x = make_block(self.block, 128*expansion_size, 256*expansion_size, list_num_residual_blocks[2])
-        
-        self.maxpool5 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv5_x = make_block(self.block, 256*expansion_size, 512*expansion_size, list_num_residual_blocks[3])
+
+        model = []
+        for idx, num_blocks in enumerate(list_num_residual_blocks):
+            if idx == 0:
+                model.append(make_block(self.block, 64, 64*expansion_size, num_blocks, stride=2))
+            else:
+                model.append(make_block(self.block, 64*expansion_size*2**(idx-1), 64*expansion_size*2**idx, num_blocks, stride=2))
+        self.model = nn.Sequential(*model)
 
         self.averagepool = nn.AvgPool2d(2)
         self.fc = nn.Linear((in_w//2**6)*(in_h//2**6)*512*expansion_size, num_classes)
     
     def forward(self, X):
         X = self.conv1(X)
-
-        X = self.maxpool2(X)
-        X = self.conv2_x(X)
-
-        X = self.maxpool3(X)
-        X = self.conv3_x(X)
-        
-        X = self.maxpool4(X)
-        X = self.conv4_x(X)
-        
-        X = self.maxpool5(X)
-        X = self.conv5_x(X)
-
+        X = self.model(X)
         X = self.averagepool(X)
         X = torch.flatten(X, start_dim=1)
         X = self.fc(X)
@@ -133,7 +127,7 @@ def ResNet101(in_channels, in_w, in_h, num_classes):
 
 def ResNet152(in_channels, in_w, in_h, num_classes):
     return ResNet(Residual_Bottle_Neck_Block, in_channels, in_w, in_h, num_classes, [3, 8, 36, 3], expansion_size=4)
-    
+
 def test(resnet):
     a = torch.randn(2, 3, 224, 224)
     model = resnet(3, 224, 224, 100)
